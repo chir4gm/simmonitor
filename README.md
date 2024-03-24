a3d263819cd4e0d455fcad82354897d197fcb704
Chirag Maheshwari <chiragm4@illinois.edu>
2024-03-23 22:37:25 -0500

    added requirements.txt; cleaned up code and made tool responsive

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-Replicates original simmonitor tool
-## Requirement(s)
-- `bokeh`
-## Usage
+Replicates original simmonitor tool. Enhanced with `bokeh` and `kuibit`.
+
+Outputs a static `index.html`, along with a `interactive.html` which uses `bokeh`
+
+## Prerequisites
+- Setup a base `texlive` install along with the `cm-super` and `ams-math` package
 ```sh
-python3 script.py --datadir [simulation directory] -v
+git clone https://github.com/chir4gm/simmonitor
+cd simmonitor
+pip3 install -r requirements.txt
+```
+
+## Usage
+- Use `simmonitor.cfg` or pass parameters to the script as command-line arguments
+
+```sh
+python3 script.py -c simmonitor.cfg
 ```
--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#! /usr/bin/python3
 import logging
 import matplotlib.pyplot as plt
 import bokeh.plotting as bkh_plt
@@ -20,23 +20,13 @@ from kuibit.visualize_matplotlib import (
 )
 from kuibit.series import sample_common
 
-config = {
-    "l" : 2,
-    "m" : 2,
-    "radius": 100,
-    'detector_num': 0,
-    "ah": [1,2],
-    "sep_ah": [1,2],
-    "ah_plane": 'yz',
-    "output_dir": "."
-}
-
-
+args = None
 logs = []
 
-bkh_plt.output_file("interactive.html")
 bkh_doc = []
 report_body = ""
+
+
 def gen_fig(fig):
     logger.debug(f"Generating Figure {fig}")
     global report_body
@@ -45,79 +35,102 @@ def gen_fig(fig):
     lines = ax.get_lines()
     x_data = [i.get_xdata() for i in lines]
     y_data = [i.get_ydata() for i in lines]
-    bkh_fig = bkh_plt.figure(width=500, height=500,title=fig,
-                              toolbar_location = 'left')
+    bkh_fig = bkh_plt.figure(title=fig, toolbar_location="left")
+
+    bkh_fig.min_border_left = 0
+    bkh_fig.min_border_right = 0
+    bkh_fig.min_border_top = 0
+    bkh_fig.min_border_bottom = 0
     bkh_fig.toolbar.logo = None
+    bkh_fig.toolbar.active_drag = None
+    bkh_fig.toolbar.active_scroll = None
+    bkh_fig.toolbar.active_tap = None
+
     for i in range(len(lines)):
         bkh_fig.line(
             x_data[i],
             y_data[i],
-            line_color = bokeh.palettes.Category10_10[i % 10],
-            line_width=2
+            line_color=bokeh.palettes.Category10_10[i % 10],
+            line_width=2,
         )
+
     bkh_doc.append(bkh_fig)
-    plt.savefig(f"{config['output_dir']}/{fig}.png")
-    plt.savefig(f"{config['output_dir']}/{fig}.pdf")
-    report_body += textwrap.dedent(f"""
-<p>
-<a href="{config['output_dir']}/{fig}.pdf"><img src="{fig}.png"/>
-        </a>
-        <br/>{fig}</p>
-    """)
+    plt.savefig(f"{args.outdir}/{fig}.png")
+    plt.savefig(f"{args.outdir}/{fig}.pdf")
+    report_body += textwrap.dedent(
+        f"""
+        <p>{fig}<br/><a href="{args.outdir}/{fig}.pdf"><img src="{fig}.png"/></a></p>
+    """
+    )
+
 
 def getTerminationReason(logfile):
     with open(logfile, "r") as fh:
         for line in fh.readlines():
-            if re.search(r"[(]TerminationTrigger[)]: Remaining wallclock time for your job is [0-9.]* minutes.  Triggering termination[.][.][.]", line):
+            if re.search(
+                r"[(]TerminationTrigger[)]: Remaining wallclock time for your job is [0-9.]* minutes.  Triggering termination[.][.][.]",
+                line,
+            ):
                 return "walltime"
-            if re.search(r"[(]NaNChecker[)]: 'action_if_found' parameter is set to 'terminate' - scheduling graceful termination of Cactus", line):
+            if re.search(
+                r"[(]NaNChecker[)]: 'action_if_found' parameter is set to 'terminate' - scheduling graceful termination of Cactus",
+                line,
+            ):
                 return "NaNChecker"
-            if re.search(r"[(]TrackTriggers[)]: Triggering termination at t=.* after surfaces .* became closer than", line):
+            if re.search(
+                r"[(]TrackTriggers[)]: Triggering termination at t=.* after surfaces .* became closer than",
+                line,
+            ):
                 return "TrackTriggers"
     return "unknown"
+
+
 def populate_logs(sim):
-    logger.debug('Populating Logs')
+    logger.debug("Populating Logs")
     sim_logs = sim.logfiles
     sim_logs.sort()
     for i in sim_logs:
         sim_num = re.search(r"output-(\d\d\d\d)", i).group(1)
-        logs.append([f'{sim_num}{"(active)" if ("active" in i) else "" }', getTerminationReason(i), i])
-        
+        logs.append(
+            [
+                f'{sim_num}{"(active)" if ("active" in i) else "" }',
+                getTerminationReason(i),
+                i,
+            ]
+        )
+
 
 def populate_graphs(sim):
+    global args
     logger.debug("Prepared SimDir")
 
     reader = sim.gravitationalwaves
 
-#    radius = config["radius"]
-#    logger.debug(f"Using radius: {radius}")
+    #    radius = args.radius
+    #    logger.debug(f"Using radius: {radius}")
+    logger.debug(f"Radii available: {reader.radii}")
+    sim_radii = None
+    if args.radius:
+        logger.debug(f"Plotting radii: {args.radius}")
+        sim_radii = args.radius
+    else:
+        logger.debug(f"No radii set. Plotting all radii")
+        sim_radii = reader.radii
 
-#   Plotting all detectors
+    for radius in sim_radii:
+        detector = reader[radius]
 
-    logger.debug(f"Radii Available: {reader.radii}")
-    for radius in reader.radii:
-        detector = reader[radius] 
-
-
-        psi4 = detector[config['l'], config['m']]
+        psi4 = detector[args.l, args.m]
         logger.debug(f"Plotting Psi4 radius={radius}")
 
-        plt.plot(
-            radius * psi4.real(),
-            label=rf"$r\Re\Psi_4^{{{config['l']}{config['m']}}}$"
-        )
-        plt.plot(
-            radius * psi4.abs(),
-            label=rf"$r|\Psi_4^{{{config['l']}{config['m']}}}|$"
-        )
+        plt.plot(radius * psi4.real(), label=rf"$r\Re\Psi_4^{{{args.l}{args.m}}}$")
+        plt.plot(radius * psi4.abs(), label=rf"$r|\Psi_4^{{{args.l}{args.m}}}|$")
 
         plt.legend()
         plt.xlabel("Time")
         plt.ylabel(r"$r \Psi_4$")
 
-        add_text_to_corner(
-            f"Det {config['detector_num']}", anchor="SW", offset=0.005
-        )
+        add_text_to_corner(f"Det {args.detector_num}", anchor="SW", offset=0.005)
         add_text_to_corner(rf"$r = {radius:.3f}$", anchor="NE", offset=0.005)
 
         set_axis_limits_from_args(args)
@@ -125,12 +138,11 @@ def populate_graphs(sim):
         gen_fig(f"psi_4_r_{radius}")
         plt.clf()
 
-
     logger.debug(
         f"Apparent horizons available: {sim.horizons.available_apparent_horizons}"
     )
 
-    for ah in config['ah']:
+    for ah in args.ah:
         if ah not in sim.horizons.available_apparent_horizons:
             raise ValueError(f"Apparent horizons {ah} is not available")
 
@@ -143,7 +155,7 @@ def populate_graphs(sim):
     # If force_com_at_origin, these objects will be rewritten so that we can
     # assume that they house the quantities we want to plot.
 
-    for ah in config['ah']:
+    for ah in args.ah:
         logger.debug(f"Reading horizon {ah}")
 
         hor = sim.horizons.get_apparent_horizon(ah)
@@ -153,7 +165,6 @@ def populate_graphs(sim):
 
     logger.debug("Reading mass")
     masses.append(hor.ah.m_irreducible)
-
 
     logger.debug("Computing center of mass")
 
@@ -166,9 +177,7 @@ def populate_graphs(sim):
     # Loop over the three coordinates and overwrite the list of
     # TimeSeries (note that ahs here are lists of TimeSeries and
     # sample_common(ahs) returns a new list of TimeSeries)
-    ah_coords = {
-        coord: sample_common(ahs) for coord, ahs in ah_coords.items()
-    }
+    ah_coords = {coord: sample_common(ahs) for coord, ahs in ah_coords.items()}
 
     masses = sample_common(masses)
 
@@ -179,24 +188,21 @@ def populate_graphs(sim):
     for coord, ah_coord in ah_coords.items():
         # For each coordinate, compute the center of mass along that
         # coordinate
-        com = sum(
-            mass * ah / total_mass
-            for mass, ah in zip(masses, ah_coords[coord])
-        )
+        com = sum(mass * ah / total_mass for mass, ah in zip(masses, ah_coords[coord]))
         # Now, we update ah_coords over that coordinate by subtracting
         # the center of mass from each apparent horizon
         ah_coord = [ah - com for ah in ah_coords[coord]]
 
-    to_plot_x, to_plot_y = config['ah_plane']
+    to_plot_x, to_plot_y = args.ah_plane
     logger.debug(f"Plotting on the x axis {to_plot_x}")
     logger.debug(f"Plotting on the y axis {to_plot_y}")
 
     # Now we loop over all the horizons
-    for ind, ah in enumerate(config['ah']):
+    for ind, ah in enumerate(args.ah):
         plt.plot(
             ah_coords[to_plot_x][ind].y,
             ah_coords[to_plot_y][ind].y,
-            label=f"Horizon {ah}"
+            label=f"Horizon {ah}",
         )
         # We save the time to plot the horizon outline
         time = ah_coords[to_plot_x][ind].tmax
@@ -218,8 +224,8 @@ def populate_graphs(sim):
     plt.clf()
 
     logger.debug("Plotting separation")
-    h1 = sim.horizons.get_apparent_horizon(config['sep_ah'][0])
-    h2 = sim.horizons.get_apparent_horizon(config['sep_ah'][1])
+    h1 = sim.horizons.get_apparent_horizon(args.sep_ah[0])
+    h2 = sim.horizons.get_apparent_horizon(args.sep_ah[1])
 
     separation = compute_separation(h1, h2, resample=True)
 
@@ -230,41 +236,54 @@ def populate_graphs(sim):
     logger.debug("Plotted")
 
     logger.debug("Saving")
-    gen_fig('sep_ah')
+    gen_fig("sep_ah")
     plt.clf()
 
-    logger.debug('Plotting norm_inf H')
-    reader = sim.timeseries['infnorm']
-    var = reader['H']
-
-    logger.debug("Plotting timeseries")
-    plt.plot(var)
-    plt.xlabel("Time")
-    plt.ylabel(r"$\|H\|_{\infty}$")
-    logger.debug("Plotted")
+    logger.debug("Plotting norm_inf H")
+    reader = sim.timeseries["infnorm"]
+    if len(reader.keys()):
+        var = reader["H"]
+        logger.debug("Plotting timeseries")
+        plt.plot(var)
+        plt.xlabel("Time")
+        plt.ylabel(r"$\|H\|_{\infty}$")
+        logger.debug("Plotted")
+        gen_fig("norm_inf_H")
+        plt.clf()
 
     logger.debug("Saving")
-    gen_fig('norm_inf_H')
+
 
 if __name__ == "__main__":
     desc = f"""{kah.get_program_name()} generates a simulation report"""
 
     parser = kah.init_argparse(desc)
-    parser.add_argument('--dark', action='store_true')
-    kah.add_figure_to_parser(parser, add_limits=True)
-    kah.add_horizon_to_parser(parser)
+    parser.add_argument("--dark", action="store_true")
+    parser.add_argument("--l", "-l", type=int)
+    parser.add_argument("--m", "-m", type=int)
+    parser.add_argument("--radius", "-r", type=int, action="append")
+    parser.add_argument("--detector_num", type=int)
+    parser.add_argument("--ah", type=int, action="append", help="Active Horizons")
+    parser.add_argument(
+        "--sep_ah", type=int, action="append", help="Separated Active Horizons"
+    )
+    parser.add_argument("--ah_plane", type=str, help="Active Horizon Plane")
     args = kah.get_args(parser)
-    setup_matplotlib({
-        'text.usetex': True,
-        'text.latex.preamble': r'\usepackage{amsmath}'
-    }, rc_par_file=args.mpl_rc_file)
+    args.xmin = None
+    args.xmax = None
+    args.ymin = None
+    args.ymax = None
 
-
+    setup_matplotlib(
+        {"text.usetex": True, "text.latex.preamble": r"\usepackage{amsmath}"}
+    )
     logger = logging.getLogger(__name__)
 
     if args.verbose:
         logging.basicConfig(format="%(asctime)s - %(message)s")
         logger.setLevel(logging.DEBUG)
+    bkh_plt.output_file(f"{args.outdir}/interactive.html")
+
     with SimDir(
         args.datadir,
         ignore_symlinks=args.ignore_symlinks,
@@ -273,9 +292,11 @@ if __name__ == "__main__":
         populate_logs(sim)
         populate_graphs(sim)
     logger.debug("Generating HTML")
-    report_file = open(f"{config['output_dir']}/index.html", 'w')
+    report_file = open(f"{args.outdir}/index.html", "w")
     sim = os.path.normpath(args.datadir)
-    report_file.write(textwrap.dedent(f"""
+    report_file.write(
+        textwrap.dedent(
+            f"""
     <html>
     <head>
         <title> {sim} overview</title>
@@ -283,14 +304,22 @@ if __name__ == "__main__":
     <body>
     <h1> {sim} overview </h1>
     {report_body}
-    """))
+    """
+        )
+    )
 
-    doc = bokeh.layouts.column(*bkh_doc)
+    doc = bokeh.layouts.column(
+        *bkh_doc,
+        sizing_mode="scale_both",
+        resizable=True,
+        spacing=10,
+        styles={"display": "block", "margin": "auto", "width": "60%"},
+    )
 
-    if (args.dark):
-        bkh_plt.curdoc().theme = 'dark_minimal'
+    if args.dark:
+        bkh_plt.curdoc().theme = "dark_minimal"
     else:
-        bkh_plt.curdoc().theme = 'light_minimal'
+        bkh_plt.curdoc().theme = "light_minimal"
 
     bkh_plt.save(doc, title="Simmonitor")
     logger.debug("DONE")
